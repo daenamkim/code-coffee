a9a7a1239c7439f2f7c739d3cb2590d835ee2892
Mia-The-Coding-Cat <mia@felixkirmse.com>
2018-10-16 00:26:22 +0000

    Added defaults

---
+++ defaults/index.js
@@ -0,0 +1,11 @@
+// Fill in undefined properties in object with the first value present in the following list of defaults objects.
+// defaults(object, *defaults);
+//
+// const aws = {compute: "ec2"};
+// defaults(aws, {compute: "lambda", storage: "s3"});
+// => {compute: "ec2", storage: "s3"}
+const defaults = () => {
+  // [SOLUTION HERE]
+};
+
+module.exports = { defaults };
