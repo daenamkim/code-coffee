7cc61132cfafc2186e34f09ee8cb5348f21e28f1
Mia-The-Coding-Cat <mia@felixkirmse.com>
2018-10-17 00:28:11 +0000

    Added difference

---
+++ difference/index.js
@@ -0,0 +1,16 @@
+// returns the values from array that are not present in the other arrays
+// difference(Array, *Others);
+// Parameters: Array, *Others
+
+// difference([1, 2, 3, 4, 5]);
+// => [1, 2, 3, 4, 5]
+// difference([1, 2, 3, 4, 5], [5, 2, 10]);
+// => [1, 3, 4]
+// difference([1, 2, 3, 4, 5], [5, 2, 10], [1, 20, 4]);
+// => [3]
+
+const difference = function(baseArray, ...others) {
+  // [SOLUTION HERE]
+};
+
+module.exports = { difference };
