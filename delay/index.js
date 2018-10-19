dbb1cd749698ddd43630c28f7c41288762b03f9c
Mia-The-Coding-Cat <mia@felixkirmse.com>
2018-10-19 00:33:13 +0000

    Added delay

---
+++ delay/index.js
@@ -0,0 +1,10 @@
+// Much like setTimeout, invokes function after wait milliseconds. If you pass the optional arguments, they will be forwarded on to the function when it is invoked.
+// delay(function, wait, *arguments);
+//
+// const notify = function (message) { alert(message); };
+// delay(notify, 1000, 'One second has passed!');
+// => 'One second has passed!' // A alert appears after one second.
+const delay = (func, wait, ...args) => {
+  // [SOLUTION HERE]
+};
+module.exports = { delay };
