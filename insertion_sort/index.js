1d430c17d0513e713c9fc06f47e770bb46bef33f
Daenam Kim <daenam.kim@gmail.com>
2018-10-24 09:57:02 +0900

    Complete insertion sort.

--- insertion_sort/index.js
+++ insertion_sort/index.js
@@ -4,8 +4,18 @@
 // insertionSort(Array);
 // insertionSort([0, -1, 2]);
 // => [-1, 0, 2]
-const insertionSort = () => {
-  // [SOLUTION HERE]
+const insertionSort = (array) => {
+  if (!Array.isArray(array)) {
+    return array;
+  }
+
+  // O(n*m).
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i; j > 0 && parseInt(array[j - 1]) > parseInt(array[i]); j--) {
+      [array[i], array[j - 1]] = [array[j - 1], array[i]];
+    }
+  }
+  return array;
 };
 
 module.exports = { insertionSort };
