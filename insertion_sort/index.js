e3d3cf3b7f0138f079a967459e3c57b83e7db4d2
controtie <dylan.ad.tran@gmail.com>
2018-10-24 09:37:41 +0900

    add insertion sort

---
+++ insertion_sort/index.js
@@ -0,0 +1,11 @@
+// Returns the sorted array using Insertion sort.
+// https://en.wikipedia.org/wiki/Insertion_sort
+//
+// insertionSort(Array);
+// insertionSort([0, -1, 2]);
+// => [-1, 0, 2]
+const insertionSort = () => {
+  // [SOLUTION HERE]
+};
+
+module.exports = { insertionSort };
