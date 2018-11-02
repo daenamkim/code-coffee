530c661421b4d882ec14746fdd246e68a7517aef
Daenam Kim <daenam.kim@gmail.com>
2018-11-02 10:00:52 +0900

    Complete traffic lights.

--- traffic_light/src/script.js
+++ traffic_light/src/script.js
@@ -12,3 +12,42 @@ Your goal is to make the best working version of a traffic light in the
 time allotted (so if you finish early, add additional
 functionality and make your code as high quality as you can!)
 */
+
+const IDLE = "";
+const RED = "red";
+const YELLOW = "yellow";
+const GREEN = "green";
+
+const updateState = (state) => {
+  switch (state) {
+    case IDLE:
+      return RED;
+    case RED:
+      return GREEN;
+    case GREEN:
+      return YELLOW;
+    case YELLOW:
+      return RED;
+    default:
+      return IDLE;
+  }
+};
+
+const updateLight = (state) => {
+  const lights = document.getElementsByTagName("circle");
+  [...lights].forEach(light => light.removeAttribute("class"));
+  document.getElementById(state).setAttribute("class", state);
+};
+
+const runSequence = (...params) => {
+  const currentState = updateState(params[0]);
+  updateLight(currentState);
+  setTimeout(runSequence, 1000, currentState);
+}
+
+window.onload = () => {
+  const button = document.getElementById("run");
+  button.addEventListener("click", () => {
+    setTimeout(runSequence, 1000, IDLE);
+  });
+};
