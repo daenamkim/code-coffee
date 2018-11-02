3e38126888a63d58f1ad7105a01fe9b12f763eb0
Mia-The-Coding-Cat <mia@felixkirmse.com>
2018-11-02 00:29:14 +0000

    Added traffic_light

---
+++ traffic_light/src/script.js
@@ -0,0 +1,14 @@
+/* YOUR CHALLENGE: Create a working traffic light.
+
+When the user clicks "run sequence", the traffic light should
+cycle through the red, yellow, and green colors.
+
+This should look like a real traffic light– colors should not overlap
+and each color should stay lit for a small amount of time.
+
+Open the index.html file in your browser to test your code.
+You may edit any of the files in this folder to complete this challenge.
+Your goal is to make the best working version of a traffic light in the
+time allotted (so if you finish early, add additional
+functionality and make your code as high quality as you can!)
+*/
