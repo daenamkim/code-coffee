fa06c89b3d83b662931ee43717bf57eff55ad32d
Daenam Kim <daenam.kim@gmail.com>
2018-12-10 10:11:20 +0900

    Complete robot paths.

--- robotPaths/index.js
+++ robotPaths/index.js
@@ -13,11 +13,61 @@ class Board {
     this.board[row][col] = !this.board[row][col];
     return this.board;
   }
+
   hasBeenVisited(row, col) {
     return this.board[row][col];
   }
 }
 
-class RobotPaths {}
+class RobotPaths {
+  constructor(size) {
+    this.size = size;
+    this.board = new Board(size);
+    this.paths = 0;
+  }
+
+  isEnd(row, col) {
+    return row === this.size - 1 && col === this.size - 1;
+  }
+
+  getDirections(row, col) {
+    const directions = [];
+
+    if (row - 1 >= 0 && !this.board.hasBeenVisited(row - 1, col)) {
+      directions.push({ row: row - 1, col });
+    }
+
+    if (row + 1 < this.size && !this.board.hasBeenVisited(row + 1, col)) {
+      directions.push({ row: row + 1, col });
+    }
+
+    if (col - 1 >= 0 && !this.board.hasBeenVisited(row, col - 1)) {
+      directions.push({ row, col: col - 1 });
+    }
+
+    if (col + 1 < this.size && !this.board.hasBeenVisited(row, col + 1)) {
+      directions.push({ row, col: col + 1 });
+    }
+
+    return directions;
+  }
+
+  solve() {
+    const findNext = (row, col) => {
+      this.board.togglePiece(row, col);
+      if (this.isEnd(row, col)) {
+        this.paths++;
+      } else {
+        const directions = this.getDirections(row, col);
+        for (const dir of directions) {
+          findNext(dir.row, dir.col);
+        }
+      }
+      this.board.togglePiece(row, col);
+    };
+    findNext(0, 0);
+    return this.paths;
+  }
+}
 
 module.exports = { RobotPaths };
--- robotPaths/spec.js
+++ robotPaths/spec.js
@@ -1,6 +1,20 @@
+const { RobotPaths } = require("./index");
 const { expect } = require("chai");
-describe("robot paths", () => {
-  it("should be something", () => {
-    expect(1).toEqual(1);
+
+describe.only("robot paths", () => {
+  it("should return a number", () => {
+    const path = new RobotPaths(3);
+    expect(path).not.to.be.undefined;
+    expect(path.solve).to.be.a("function");
+    const result = path.solve();
+    expect(result).to.be.a("number");
+  });
+
+  it("should return a number", () => {
+    const path = new RobotPaths(2);
+    expect(path).not.to.be.undefined;
+    expect(path.solve).to.be.a("function");
+    const result = path.solve();
+    expect(result).to.be.eql(2);
   });
 });
