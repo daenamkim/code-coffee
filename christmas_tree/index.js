7397e22a7d6b761d66bf093cc7c5ebbb5a996478
Daenam Kim <daenam.kim@gmail.com>
2018-12-17 09:32:12 +0900

    Complete christmas tree.

--- christmas_tree/index.js
+++ christmas_tree/index.js
@@ -1,5 +1,25 @@
-const christmasTree = () => {
-  // FIXME: fill me out!
+/***
+ * Input is  number, string
+ * i.e. 3, '$'
+ * Output is
+ *   $\n
+ *  $ $\n
+ * $ $ $
+ */
+
+const christmasTree = (num, char) => {
+  if (!num || !char) {
+    return "";
+  }
+
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    const prefix = new Array(num - 1 - i).fill(" ").join("");
+    const line = new Array(i + 1).fill(char).join(" ");
+    result += `${prefix}${line}\n`;
+  }
+
+  return result;
 };
 
 module.exports = { christmasTree };
--- christmas_tree/spec.js
+++ christmas_tree/spec.js
@@ -1,7 +1,12 @@
 const { expect } = require("chai");
+const { christmasTree } = require("./index");
 
-describe("Christmas Tree", () => {
-  it("should be true", () => {
-    expect(true).toBeTruthy();
+describe.only("Christmas Tree", () => {
+  it("should be a function", () => {
+    expect(typeof christmasTree).to.equal("function");
+  });
+
+  it("should return a tree", () => {
+    expect(christmasTree(3, "$")).to.equal("  $\n $ $\n$ $ $\n");
   });
 });
